4609acdd2e7251d6072284840ef30aeeee8e905d
Steven Quan <63191202+wizardap@users.noreply.github.com>
2021-05-27 00:35:53 +0700

    Update convexhulltrick.cpp

--- DP Optimize/convexhulltrick.cpp
+++ DP Optimize/convexhulltrick.cpp
@@ -1,51 +1,85 @@
 ///All template are helped to find maximum value at point x in line y=a*x+b
 
 
-//// Based Codeforces users : xfce66666
-struct ConvexHull {
+//// Based on Codeforces users : xfce66666
+/// Modified by WizardAP
+/// Use to query maximum value, if u wanna query minimum query , just change sign to (-) . Example : y=ax+b -> add(a,b), query(x) . After change sign , it will be : add(-a,-b),-query(x)
+struct ConvexHull 
+{
+	int size = 0;
 	struct Line
 	{
 		LL a, b;
-		Line() : a(0), b(0) {}
-		Line(LL a, LL b) : a(a), b(b) {}
-		LL eval(LL x) { return a * x + b;}
+		Line(): a(0), b(0) {};
+		Line(LL a, LL b): a(a), b(b) {};
+		LL eval(LL x) {return (a * x + b);}
 	};
-	int size = 0;
+	struct roll_back
+	{
+		int pos, top;
+		Line overwrite;
+		roll_back(): pos(0), top(0), overwrite(Line(0, 0)) {};
+		roll_back(int p, int t, Line o): pos(p), top(t), overwrite(o) {};
+	};
+
 	vector<Line> hull;
-	ConvexHull(int maxSize) {
-		hull.resize(maxSize++, Line(0, 0));
+	vector<roll_back> backup;
+	void restart(int maxSize)
+	{
+		hull.resize(maxSize + 5, Line(0, 0));
 	}
-	LL Div( LL a, LL b) {
-		return a / b - (a % b && ((a < 0) ^ (b < 0) )) ;
+	void undo()
+	{
+		roll_back prev = backup.back();
+		backup.pop_back();
+		size = prev.top;
+		hull[prev.pos] = prev.overwrite;
 	}
-	bool is_bad(int curr, int prev, int next) {
-		Line c = hull[curr], p = hull[prev], n = hull[next];
-		return Div(c.b - n.b, n.a - c.a) <= Div(p.b - c.b, c.a - p.a);
+	LL Div(LL a, LL b)
+	{
+		return (a / b) - ((a ^ b) < 0 && a % b);
 	}
+	bool bad(Line cur, Line prev, Line next) {
+		return Div(cur.b - next.b, next.a - cur.a) <= Div(prev.b - cur.b, cur.a - prev.a);
+	}
+	void insert(LL k, LL m)/// if u don't or can't sort slope
+	{
 
-
-	void add(LL a, LL b) {
-		hull[size++] = (Line) {a, b};
-		while (size > 2 && is_bad(size - 2, size - 3, size - 1))
+		Line p = Line(k, m);
+		int l = -1, r = size - 1;
+		while (r - l > 1)
+		{
+			int mid = (l + r) >> 1;
+			if (!bad(hull[mid + 1], hull[mid], p)) l = mid;
+			else r = mid;
+		}
+		int idx = r + 1;
+		backup.pb(roll_back(idx, size, hull[idx]));
+		size = idx;
+		hull[size++] = p;
+	}
+	void add(LL k, LL m) /// if all slopes are sorted
+	{
+		hull[size++] = Line(k, m);
+		while (size > 2 && bad(hull[size - 2], hull[size - 3], hull[size - 1]))
 		{
 			hull[size - 2] = hull[size - 1];
 			size--;
 		}
 	}
-
-	LL query(LL x) {
+	LL query(LL x)
+	{
 		int l = -1, r = size - 1;
-		while (r - l > 1) {
-			int m = (l + r) / 2;
-			if (hull[m].eval(x) <= hull[m + 1].eval(x))
-				l = m;
-			else
-				r = m;
+		while (r - l > 1)
+		{
+			int mid = (l + r) >> 1;
+			if (hull[mid].eval(x) <= hull[mid + 1].eval(x)) l = mid;
+			else r = mid;
 		}
 		return hull[r].eval(x);
 	}
-};
-//// Short code from KACTL. Be careful !  This template can only be used from c++14 and above.
+} cht;
+//// Short code from KACTL. Be careful !  This template can only be used from c++14 and above. And this code can't delete or roll back 
 struct Line {
 	mutable LL k, m, p;
 	bool operator<(const Line& o) const { return k < o.k; }
