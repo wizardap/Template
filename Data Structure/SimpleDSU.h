9b67418d2365029e0d9ccb84d158e20e71b701c3
Steven Quan <63191202+wizardap@users.noreply.github.com>
2021-08-17 20:10:49 +0700

    Update 0-based index

--- Data Structure/SimpleDSU.h
+++ Data Structure/SimpleDSU.h
@@ -3,28 +3,21 @@
     Verification:
         - https://acm.timus.ru/problem.aspx?space=1&num=1671
 */
-///1-based
+///0-based or 1-based is okay 
 struct DSU
 {
-    vector<int> par, rnk;
-    int n;
-    void init_dsu(int N)
+    vector<int> p, size;
+    DSU(int n = 0): p(n + 1, -1), size(n + 1, 1) {};
+    int root(int x) { return p[x] == -1 ? x : p[x] = root(p[x]);}
+    bool unite(int u, int v)
     {
-        n = N;
-        par.assign(n + 1, 0);
-        rnk.assign(n + 1, 0);
-        for (int i = 1; i <= n; i++) par[i] = i;
-    }
-    int Find(int u) {return u == par[u] ? par[u] : par[u] = Find(par[u]);}
-    bool same(int u, int v) {return Find(u) == Find(v);}
-    bool Unite(int u, int v)
-    {
-        u = Find(u);
-        v = Find(v);
+        u = root(u);
+        v = root(v);
         if (u == v) return false;
-        if (rnk[u] < rnk[v]) swap(u, v);
-        par[v] = u;
-        if (rnk[u] == rnk[v]) rnk[u]++;
+        if (size[u] < size[v]) swap(u, v);
+        size[u] += size[v];
+        p[v] = u;
         return true;
     }
-} dsu;
+    bool same(int u, int v) { return (root(u) == root(v));}
+};
