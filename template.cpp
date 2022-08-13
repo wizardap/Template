54001978991f84365ca737f757e8d12edb050c18
Steven Quan <63191202+wizardap@users.noreply.github.com>
2022-08-14 00:18:23 +0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -11,9 +11,14 @@ using namespace std;
 #define vt                      vector
 #define all(x)                  (x).begin(),(x).end()
 #define aln(x,y)                (x)+1,(x)+1+(y)
+#define cntone(x)          __builtin_popcountll((x))
+#define each(x,y)          for (auto &x: y)
 #define KwanJi                  freopen(Nothing".inp","r",stdin),freopen(Nothing".out","w",stdout)
 #define Mitsuha                 ios_base::sync_with_stdio(false),cin.tie(nullptr),cout.tie(nullptr)
 
+template<class T, class Y> T offbit(T mask, Y pos) { return (mask & ~((T)1 << pos));}
+template<class T, class Y> T onbit(T mask, Y pos) {return (mask | ((T)1 << pos));}
+template<class T, class Y> T getbit(T mask, Y pos) {return ((mask >> (T)pos) & (T)1);}
 template<class T1, class T2> bool ckmin(T1 &x, T2 y) { return (x > y ? x = y, 1 : 0);}
 template<class T1, class T2> bool ckmax(T1 &x, T2 y) { return (x < y ? x = y, 1 : 0);}
 mt19937_64 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
@@ -25,16 +30,18 @@ const ll mod = 1e9 + 7;
 const ll inf = 1e18;
 const int INF = 1e9;
 const ld eps = 1e-9;
+const ld PI = acos(-1.0);
 ll fmul(ll a, ll b, ll MOD = mod) {a %= MOD; b %= MOD; ll q = (ld)a * b / MOD; ll r = a * b - q * MOD; return (r % MOD + MOD) % MOD;}
 ///Head
+
 void Solve()
 {
-      
+   
 }
 signed main()
 {
    Mitsuha;
-   int TC;
+   int TC=1;
    cin >> TC;
    while (TC--) Solve();
 
